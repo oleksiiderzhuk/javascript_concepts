8a20eb749468d311e88e3fe832ab0802d66308ab
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2023-09-11 15:31:07 +0100

    Fake namespaces

---
+++ 52.1_fakeNamespaces/js/app.js
@@ -0,0 +1,4 @@
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+
---
+++ 52.1_fakeNamespaces/js/script2.js
@@ -0,0 +1,6 @@
+  var johnGreeter = {};
+  johnGreeter.name = "John";
+  johnGreeter.sayHi = function () {
+    console.log("Hi " + johnGreeter.name);
+  }
+
